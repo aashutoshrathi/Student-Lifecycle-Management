edeefd485a3db8795d45214d0c2ebcd3c55b5ebb
Monal5031 <201652013@iiitvadodara.ac.in>
2017-10-07 15:27:52 -0700

    updated urls

---
+++ portal/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import login_view
+
+urlpatterns = [
+    url(r'^$', login_view(), name='login'),
+    url(r'login.html$', login_view(), name='login'),
+
+]
