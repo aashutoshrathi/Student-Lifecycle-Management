88265412eaec79f8fc109d56e8b4474b86f9f598
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-10-08 00:04:10 +0530

    Added Student app

---
+++ student/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Student(models.Model):
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+    GENDER_CHOICES = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+        ('o', 'Other'),
+    )
+
+    MARITAL_STATUS = (
+        ('m', 'Married'),
+        ('u', 'Unmarried'),
+    )
+
+    name = models.CharField(max_length=30, blank=True, null=True)
+    student_id = models.CharField(max_length=9, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Reg(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_code = models.CharField(max_length=10, blank=True, null=True)
+    attendance = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.course_code
