fa89fda1a7948bb6d87e3227f86f09d862ebcfad
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-10-08 15:09:09 +0530

    Added teacher views

--- teacher/views.py
+++ teacher/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Teacher
+from .models import Teacher, AssignedCourse
 
 
 def teacher_profile_view(request, teacher_id):
     teacher = get_object_or_404(Teacher, teacher_id=teacher_id)
-    context = {'teacher': teacher}
+    all_assigned_courses = AssignedCourse.objects.filter(teacher=teacher)
+    context = {
+        'teacher': teacher,
+        'all_assigned_courses': all_assigned_courses
+    }
     return render(request, 'teacher/teacher.html', context=context)
